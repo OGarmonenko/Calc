86589c8efc4704c1c02570474fb12fef0b48c9b8
Olga <olga_bodak@list.ru>
2021-08-16 19:58:35 +0300

    fixed errors

--- main.js
+++ main.js
@@ -22,8 +22,9 @@ function setInputValue(e) {
   flag = setInput(tmp, flag);
 }
 function setInput(item, flag) {
-  console.log("item", item);
-  if (!flag && /\D/.test(item)) {
+  //если не число, разрешать работать с полученным результатом, иначе выводим новое значение
+  const tmp = /\D/.test(item);
+  if (!flag && tmp) {
     flag = true;
   }
   input.value = flag ? input.value + item : "" + item;
@@ -34,7 +35,7 @@ function changeSign() {
   const num = tmp[tmp.length - 1];
   const index = input.value.lastIndexOf(`${num}`);
   const newValue =
-    index === 0 ? `${num * -1}` : input.value.slice(0, index) + `(${num * -1})`;
+    index === 0 ? `${num * -1}` : `${input.value.slice(0, index)}${num * -1}`;
   input.value = newValue;
 }
 function deleteEnd() {
@@ -47,16 +48,64 @@ function deleteAll() {
   input.value = "";
 }
 function getSolution() {
-  let result = eval(input.value);
+  let result = calculate(input.value);
   labelSolution.innerText =
-    result == "Infinity" ? "Делить на ноль нельзя!" : input.value + "=";
+    result == "Infinity"
+      ? "Делить на ноль нельзя!"
+      : result == "Error"
+      ? "Неправильное выражение!"
+      : input.value + "=";
   labelSolution.style.visibility = "visible";
   input.value =
-    result == "Infinity" ? input.value : Math.round(result * 1000) / 1000;
+    result == "Infinity" || result == "Error"
+      ? input.value
+      : Math.round(result * 1000) / 1000;
   flag = false;
 }
+function calculate(str) {
+  const arr = str.match(/(\-?\d+\.?(\d)*|([-+*/]?))/g);
+  //проверяем input.value на корректность
+  let err = test(arr, str);
+  if (err) return "Error";
+  let newValue;
+  for (let i = 0; i < arr.length - 1; ) {
+    newValue =
+      i === 0 ? +arr[i] : getCurrentValue(+newValue, arr[i], arr[i + 1]);
+    i = /\-?\d+\.?(\d)*/.test(arr[i]) ? (i += 1) : (i += 2);
+  }
+  return newValue;
+}
+function getCurrentValue(num1, op, num2) {
+  if (!["+", "-", "/", "*"].includes(op)) {
+    return num1 + +op;
+  }
+  switch (op) {
+    case "+":
+      return num1 + +num2;
+    case "-":
+      return num1 - num2;
+    case "/":
+      return num1 / num2;
+    case "*":
+      return num1 * num2;
+    default:
+      break;
+  }
+}
+function test(arr, str) {
+  let err = 0;
+  const lastCharNotNumber = /\D/.test(str[str.length - 1]);
+  if ([...arr].join("").length !== str.length) {
+    err = 1;
+  }
+  if (lastCharNotNumber) {
+    err = 2;
+  }
+  return err;
+}
 function getEventKey(e) {
-  if (/^(\d+|([-*+/\.])?)$/g.test(e.key)) {
+  let itemCorrect = /^(\d+|([-*+/\.])?)$/g.test(e.key); // числа или матем.знаки
+  if (itemCorrect) {
     labelSolution.style.visibility = "hidden";
     let tmp = e.key;
     flag = setInput(tmp, flag);
